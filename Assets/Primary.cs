e864eedee1ade7b31fbd030a463f118a391073b7
DESKTOP-T61VKPT\priemg <gabrielpriem@gmail.com>
2019-04-15 18:30:45 -0400

    Created a player sprite and split the sprite sheet; created Primary.cs for the player...

---
+++ Assets/Primary.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Primary : MonoBehaviour
+{
+    [SerializeField] float playerSpeed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Translate(Vector2.right * playerSpeed * Time.deltaTime);
+        }
+    }
+}
