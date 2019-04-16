3967bbb182c9cc7dca7c05b97c99b56d61421826
DESKTOP-T61VKPT\priemg <gabrielpriem@gmail.com>
2019-04-16 11:22:26 -0400

    Enemy does not pace back and forth correctly yet; pivoting platform moves correctly...

---
+++ Assets/BasicEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    [SerializeField] float maxDistance = 3.0f;
+    [SerializeField] float enemySpeed = 2.0f;
+    private bool isMovingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMovingRight == true) { gameObject.transform.Translate(Vector2.right * enemySpeed * Time.deltaTime); }
+        else if (isMovingRight == false) { gameObject.transform.Translate(Vector2.left * enemySpeed * Time.deltaTime); }
+        if (gameObject.transform.position.x >= maxDistance + gameObject.transform.position.x) { isMovingRight = false; }
+        else if (gameObject.transform.position.x >= maxDistance - gameObject.transform.position.x) { isMovingRight = true; }
+        //gameObject.transform.Translate(Vector2.right * playerSpeed * Time.deltaTime);
+        //if (gameObject.transform.position.x >= maxDistance) { gameObject.transform.Translate(Vector2.left * enemySpeed * Time.deltaTime); }
+        //else if (gameObject.transform.position.x <= -maxDistance) { gameObject.transform.Translate(Vector2.right * enemySpeed * Time.deltaTime); }
+
+    }
+}
