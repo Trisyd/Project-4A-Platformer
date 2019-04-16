0271c3b4a8849ac8a1d39b0acc95417dad949d38
DESKTOP-T61VKPT\priemg <gabrielpriem@gmail.com>
2019-04-16 11:29:59 -0400

    Enemy pacing works properly...

--- Assets/BasicEnemy.cs
+++ Assets/BasicEnemy.cs
@@ -7,11 +7,12 @@ public class BasicEnemy : MonoBehaviour
     [SerializeField] float maxDistance = 3.0f;
     [SerializeField] float enemySpeed = 2.0f;
     private bool isMovingRight = true;
+    private float staticPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        staticPosition = gameObject.transform.position.x;
     }
 
     // Update is called once per frame
@@ -19,8 +20,8 @@ public class BasicEnemy : MonoBehaviour
     {
         if (isMovingRight == true) { gameObject.transform.Translate(Vector2.right * enemySpeed * Time.deltaTime); }
         else if (isMovingRight == false) { gameObject.transform.Translate(Vector2.left * enemySpeed * Time.deltaTime); }
-        if (gameObject.transform.position.x >= maxDistance + gameObject.transform.position.x) { isMovingRight = false; }
-        else if (gameObject.transform.position.x >= maxDistance - gameObject.transform.position.x) { isMovingRight = true; }
+        if (gameObject.transform.position.x >= maxDistance + staticPosition) { isMovingRight = false; }
+        else if (gameObject.transform.position.x <= staticPosition - maxDistance) { isMovingRight = true; }
         //gameObject.transform.Translate(Vector2.right * playerSpeed * Time.deltaTime);
         //if (gameObject.transform.position.x >= maxDistance) { gameObject.transform.Translate(Vector2.left * enemySpeed * Time.deltaTime); }
         //else if (gameObject.transform.position.x <= -maxDistance) { gameObject.transform.Translate(Vector2.right * enemySpeed * Time.deltaTime); }
